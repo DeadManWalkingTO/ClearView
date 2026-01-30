190fcd2477888ee1cd988dd46d7974682fb42cf2
DeadManWalking <deadmanwalkingto@gmail.com>
2026-01-30 20:28:09 +0200

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,5 +1,8 @@
 @echo off
 pushd %~dp0
-.\external\AutoHotkey64.exe .\submacros\main.ahk
-echo ExitCode=%errorlevel%
-pause
+
+REM Εκκίνηση της εφαρμογής AHK ως ανεξάρτητης διεργασίας και έξοδος του .bat αμέσως
+start "" ".\external\AutoHotkey64.exe" ".\submacros\main.ahk"
+
+popd
+exit /b
